36b4244c4c0ca43d39809ecfc1a4ccfefa62ae82
dan9112 <58068292+dan9112@users.noreply.github.com>
2021-02-10 08:07:37 +0300

    added a simple example with minor edits by developer: output to the log instead of the console

--- 01-start/src/main/java/ru/test/a01_start/MainActivity.kt
+++ 01-start/src/main/java/ru/test/a01_start/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.test.a01_start
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+
+        GlobalScope.launch { // запустить новую сопрограмму в фоновом режиме и продолжить
+            delay(1000L) // неблокирующая задержка на 1 секунду (единица времени по умолчанию - мс)
+            Log.d("myLog", "World!") // добавить в строку после задержки
+        }
+        Log.d("myLog", "Hello,") // основной поток продолжается, пока сопрограмма задерживается
+        Thread.sleep(2000L) // заблокировать основной поток на 2 секунды, чтобы JVM оставалась в живых
     }
 }

971a8705f87a1d3fed82942875f42f19afce81b3
dan9112 <58068292+dan9112@users.noreply.github.com>
2021-02-11 11:07:57 +0300

    added interrupt to timer example

--- 14_1-timeout/src/main/java/ru/test/a14_1_timeout/MainActivity.kt
+++ 14_1-timeout/src/main/java/ru/test/a14_1_timeout/MainActivity.kt
@@ -7,20 +7,32 @@ import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var jobScope: CoroutineScope
+
     override fun onCreate(savedInstanceState: Bundle?) = runBlocking {
         super.onCreate(savedInstanceState)
 
+        jobScope = CoroutineScope(coroutineContext)
+
+        launch {
+            delay(7100L)
+            jobScope.coroutineContext.cancel()
+            Log.v("myLog", "job_0: Отмена области действий jobScope.")
+        }
+
         try {
-            withTimeout(1300L) {
-                repeat(1000) { i ->
-                    Log.d("myLog", "Я сплю $i ...")
-                    delay(500L)
+            jobScope.coroutineContext.let {
+                withTimeout(9300L) {
+                    repeat(1000) { i ->
+                        Log.v("myLog", "job_1: Я сплю $i ...")
+                        delay(500L)
+                    }
                 }
             }
         } catch (exception: TimeoutCancellationException) {
-            Log.d("myLog", "Время вышло!")
+            Log.v("myLog", "job_1: Время вышло!")
         } catch (exception: CancellationException) {
-            Log.d("myLog", "Корутина отменена из-вне!")
+            Log.v("myLog", "job_1: Корутина отменена извне!")
         }
         onDestroy()
     }

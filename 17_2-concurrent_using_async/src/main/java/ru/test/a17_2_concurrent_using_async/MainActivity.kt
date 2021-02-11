83905c9a78913035f69580c326d9bbbe8e285876
dan9112 <58068292+dan9112@users.noreply.github.com>
2021-02-11 15:56:47 +0300

    added example with asynchronous call of suspend functions

---
+++ 17_2-concurrent_using_async/src/main/java/ru/test/a17_2_concurrent_using_async/MainActivity.kt
@@ -0,0 +1,30 @@
+package ru.test.a17_2_concurrent_using_async
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) = runBlocking<Unit> {
+        super.onCreate(savedInstanceState)
+
+        val time = measureTimeMillis {
+            val one = async { doSomethingUsefulOne() }
+            val two = async { doSomethingUsefulTwo() }
+            Log.v("myLog", "Ответ: ${one.await() + two.await()}")
+        }
+        Log.v("myLog", "Завершено за $time мс")
+    }
+
+    private suspend fun doSomethingUsefulOne(): Int {
+        delay(1000L)
+        return 13
+    }
+
+    private suspend fun doSomethingUsefulTwo(): Int {
+        delay(1000L)
+        return 29
+    }
+}
